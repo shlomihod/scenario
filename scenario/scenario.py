65e951788d813121c7c34f2cc192cb697033c359
shlomi_h <hod_s@bezeqint.net>
2016-08-15 01:54:18 +0200

    Re-committing previous changes

--- scenario/scenario.py
+++ scenario/scenario.py
@@ -42,6 +42,7 @@ def parse_scenario_file(scenario_path):
 
 def play_scenario(scenario, executable_path):
 
+    result = None
     feedback = ''
 
     executable_path_with_args = executable_path + ' '+ scenario['args']
@@ -52,12 +53,17 @@ def play_scenario(scenario, executable_path):
 
     try:
         for index, (actor, quote) in enumerate(scenario['dialog']):
+            
             if actor == 'O':
                 p.expect_exact(quote)
+
                 if p.before.strip('\r\n') != '':
                     raise pexpect.TIMEOUT(TIMEOUT)
 
             elif actor == 'I':
+                if not p.isalive():
+                    raise pexpect.EOF(TIMEOUT)
+
                 p.sendline(quote)
 
             feedback += 'ok {!r}\n'.format(quote)
@@ -66,19 +72,21 @@ def play_scenario(scenario, executable_path):
         feedback += 'not ok TOO EARLY END OF EXECUTION\n'
         feedback += '---at {!r}\n'.format(quote)
         feedback += 'FAILED!\n'
+        result = False
 
     except pexpect.TIMEOUT:
         feedback += 'not ok MISMATCH\n'
         feedback += '---should be {!r}\n'.format(quote)
         feedback += '---but got   {!r}\n'.format(p.before.strip('\r\n'))
         feedback += 'FAILED!\n'
+        result = False
 
     else:
         try:
             p.expect(pexpect.EOF)
 
             if p.before.strip('\r\n'):
-                raise  pexpect.TIMEOUT(TIMEOUT)
+                raise pexpect.TIMEOUT(TIMEOUT)
 
         except pexpect.TIMEOUT:
             feedback += 'not ok EXPECTED END OF EXECUTION\n'
@@ -87,15 +95,17 @@ def play_scenario(scenario, executable_path):
                 feedback += '---print after {!r}\n'\
                             .format(p.before.strip('\r\n'))
             feedback += 'FAILED!\n'
+            result = False
 
         else:
             feedback += 'exit code {}\n'.format(p.exitstatus)
             feedback += 'SUCCEED!\n'
+            result = True
 
-    return feedback
+    return result, feedback
 
 def run_scenario(executable_path, scenario_path):
     scenario = parse_scenario_file(scenario_path)
-    feedback = play_scenario(scenario, executable_path)
+    result, feedback = play_scenario(scenario, executable_path)
 
-    print feedback
+    return result, feedback
