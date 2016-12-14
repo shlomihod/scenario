6872a6c633ad7e1e89f10a4d824c3506c86d927f
shlomi_h <hod_s@bezeqint.net>
2016-12-14 17:08:42 +0100

    updating version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='scenario',
-      version='0.2.0',
+      version='0.3.0',
       packages=['scenario'],
       install_requires=['pexpect'],
       entry_points={
