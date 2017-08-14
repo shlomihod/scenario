298880ed911c9965ad5b06e556bd2539a5989897
shlomi_h <hod_s@bezeqint.net>
2017-08-14 17:45:00 +0200

    correcrting __all__

--- scenario/parser/__init__.py
+++ scenario/parser/__init__.py
@@ -1,7 +1,4 @@
-__all__ = ['parse_scenario_json',
-           'ParserJSONLoadingError',
-           'ParserJSONValidationError']
+__all__ = ['parse_scenario_json', ' ParserError']
 
 from scenario.parser.parser import parse_scenario_json
-from scenario.parser.exceptions import ParserJSONLoadingError, \
-                                       ParserJSONValidationError
+from scenario.parser.exceptions import ParserError
