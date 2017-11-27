b6de2d7035736042189917e062e1701c0015a6b6
shlomi_h <hod_s@bezeqint.net>
2017-11-27 17:52:31 +0100

    api works with paths

--- scenario/api/__init__.py
+++ scenario/api/__init__.py
@@ -1,16 +1,16 @@
 import json
-import copy
 
 import jsonschema
 
+from scenario.parser import parse_scenario_json
 from scenario.player.feedback_exceptions import TimeoutFeedbackError, \
     OverflowFeedbackError
 from scenario.utils import get_result_dict, get_feedback_dict
 from scenario.consts import FEEDBACK_JSON_SCHEMA
 
 
-def _get_feedback_json(scenario, feedback_exception):
-    feedback = copy.deepcopy(scenario)
+def _get_feedback_json(scenario_path, feedback_exception):
+    feedback = parse_scenario_json(scenario_path)
     feedback['log'] = {'quotes': [], 'text': ''}
     feedback['result'] = get_result_dict(False)
     feedback['feedback'] = get_feedback_dict(feedback_exception())
@@ -21,9 +21,9 @@ def _get_feedback_json(scenario, feedback_exception):
     return json.dumps(feedback, indent=2, sort_keys=True)
 
 
-def get_timeout_feedback_json(scenario):
-    return _get_feedback_json(scenario, TimeoutFeedbackError)
+def get_timeout_feedback_json(scenario_path):
+    return _get_feedback_json(scenario_path, TimeoutFeedbackError)
 
 
-def get_overflow_feedback_json(scenario):
+def get_overflow_feedback_json(scenario_path):
     return _get_feedback_json(scenario, OverflowFeedbackError)
